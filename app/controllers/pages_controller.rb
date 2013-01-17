162d0872672eea7d5eaf6be2f0d3d57f82685b70
Samir Merchant <fanged.parakeet@gmail.com>
2013-01-16 21:25:10 -0600

    building in ajax guessing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,12 +5,26 @@ class PagesController < ApplicationController
   end
   
   def play
-    @guess = params[:guess]
+    @guess = params[:guess].downcase
     @answer = unguessed.word
     @message = above_or_below(@answer, @guess)
+    @count = 1
   end
   
   def guess
+    @guess = params[:guess].downcase
+    @count = params[:num].to_i + 1
+    @answer = unguessed.word
+    @message = above_or_below(@answer, @guess)
+    respond_to do |format|
+      format.js
+    end
+  end
+  
+  def correct
+    name = params[:name]
+    name = "Anon" unless params[:name]
+    word = params[:word]
     
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ WordGameApp::Application.routes.draw do
   root to: "pages#home"
   
   post "play" => "pages#play", as: :play
+  post "guess" => "pages#guess", as: :guess
+  post "correct" => "pages#correct", as: :correct
   
 end
